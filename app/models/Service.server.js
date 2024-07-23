093f1ced050ddb51a7919ffa7c94e51de7838e3e
Ahmet Selim Alpkirişçi <salpkirisci@gmail.com>
2024-07-23 15:58:53 +0300

    tested GraphQL customer scope

--- app/models/Service.server.js
+++ app/models/Service.server.js
@@ -1,2 +1,41 @@
 import db from "../db.server";
 
+async function getCustomerId(email, graphql) {
+    if (!email) {
+        throw new Error("Email is null");
+    }
+
+    const response = await graphql(
+        `
+        query getCustomerId($query: String!) {
+            customers(first: 1, query: $query) {
+                edges {
+                    node {
+                        id
+                        firstName
+                        lastName
+                        email
+                    }
+                }
+            }
+        }
+        `,
+        {
+            variables: {
+                query: `email:${email}`, // Search customers by email
+            }
+        }
+    );
+
+    const { data: { customers } } = await response.json();
+    const customer = customers.edges[0]?.node;
+
+    return {
+        customerEmail: email,
+        customerId: customer?.id,
+        customerFirstName: customer?.firstName,
+        customerLastName: customer?.lastName
+    };
+}
+
+export { getCustomerId };
